936299c348df248ad5f150192e3ac11be3ba1423
beomgyu.heo <0105086@naver.com>
2024-05-16 16:41:09 +0900

    PrimeFactor1차 commit

---
+++ Project4/PrimeFactor.cpp
@@ -0,0 +1,23 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+class PrimeFactor
+{
+private:
+	int balance;
+
+public:
+	PrimeFactor() :balance() {
+
+	}
+	vector<int> getFactors(int value)
+	{
+		vector<int> ret;
+		if (value == 2)
+		{
+			ret.push_back(2);
+		}
+		return ret;
+	}
+};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,24 +1,8 @@
-#include "pch.h"
-#include "../Project4/cal.cpp"
+﻿#include "pch.h"
+#include "../Project4/PrimeFactor.cpp"
 
-class AccountFixture : public testing::Test
-{
-public:
-	Account acount{ 10000 };
-};
-
-TEST_F(AccountFixture, TestName) {
-	acount.deposit(10000);
-	EXPECT_EQ(20000, acount.getBalance());
+TEST(TestCaseName, TestName) {
+	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
 }
 
-TEST_F(AccountFixture, TestName2) {
-	acount.deposit(100);
-	EXPECT_EQ(10100, acount.getBalance());
-
-	acount.withDraw(200);
-	EXPECT_EQ(9900, acount.getBalance());
-
-	EXPECT_TRUE(true);
-}
