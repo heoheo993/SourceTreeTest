6bd87abddefd0959001cca207b8825190ebb5cce
beomgyu.heo <0105086@naver.com>
2024-05-16 17:03:07 +0900

    PrimeFactor 함수 1차 완성

--- Project4/PrimeFactor.cpp
+++ Project4/PrimeFactor.cpp
@@ -13,23 +13,19 @@ public:
 	}
 	vector<int> getFactors(int val)
 	{
-		vector<int> ret;
-		if (val > 1)
-		{
-			while (val % 2 == 0)
-			{
-				ret.push_back(2);
-				val /= 2;
-			}
-			while (val % 3 == 0)
-			{
-				ret.push_back(3);
-				val /= 3;
-			}
-		}
-		else
-		{
+		if (val <= 1) 
 			return { 1 };
+
+		vector<int> ret;
+		int Originval = val;
+		for (int DivValue = 2; DivValue <= Originval; DivValue++) {
+			while (val % DivValue == 0)
+			{
+				ret.push_back(DivValue);
+				val /= DivValue;
+				if (val == 1)
+					return ret;
+			}
 		}
 
 		return ret;
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -15,18 +15,32 @@ TEST(TestCaseName, TestName) {
 	exp = { 2,2 };
 	EXPECT_EQ(exp, PrimeFactor().getFactors(4));
 
-	//exp = { 5 };
-	//EXPECT_EQ(exp, PrimeFactor().getFactors(5));
+	exp = { 5 };
+	EXPECT_EQ(exp, PrimeFactor().getFactors(5));
 
 	exp = { 2,3 };
 	EXPECT_EQ(exp, PrimeFactor().getFactors(6));
 
-	//exp = { 7};
-	//EXPECT_EQ(exp, PrimeFactor().getFactors(7));
+	exp = { 7};
+	EXPECT_EQ(exp, PrimeFactor().getFactors(7));
 
 	exp = { 2,2,2 };
 	EXPECT_EQ(exp, PrimeFactor().getFactors(8));
 
+	exp = {3,3 };
+	EXPECT_EQ(exp, PrimeFactor().getFactors(9));
+
+	exp = { 2,5};
+	EXPECT_EQ(exp, PrimeFactor().getFactors(10));
+
+	exp = { 11 };
+	EXPECT_EQ(exp, PrimeFactor().getFactors(11));
+
+	exp = { 2,2,3 };
+	EXPECT_EQ(exp, PrimeFactor().getFactors(12));
+
+	exp = { 73, 137 };
+	EXPECT_EQ(exp, PrimeFactor().getFactors(10001));
 
 	EXPECT_TRUE(true);
 }
